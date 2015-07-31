5104bf4e6a0a5baf251ffe9088cef42d88330bd3
bindkeeper <bindkeeper@gmail.com>
2015-07-31 12:43:09 +0300

    added summary functionality now when the script is done: 1) For each return code it prints how many times that return code was returned 2) It print the most frequent return code

--- pingize.sh
+++ pingize.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+function summary {
+	echo -e '\n'======summary======='\n'
+	# print how many times each return code returned and print the most frequesnt one
+	most_key=
+	most_value=
+	for i in "${!RETURN_CODES[@]}"
+	do
+		value=${RETURN_CODES[$i]}
+		if [[ $most_value < $value ]]
+		then 
+			most_value=$value
+			most_key=$i
+		fi
+		echo the return code "${i}" was "${RETURN_CODES[$i]}" times
+		
+	done
+	echo -e '\n'the most frequent return code is "${most_key}"
+	echo -e '\n'===end of summary===
+	exit $1
+}
+
 function usage {
 	echo Usage: pingize.sh [Options] -cmd COMMAND
 	echo -e '\t'	-c NUMBER '\t'	the number of times to perform a command
@@ -38,6 +59,10 @@ do
 done
 #end of read user parameters
 
+declare -A RETURN_CODES # declare a hash table taht will hold the return codes as keys an return codes as values
+
+
+
 if [[ -z $CMD ]]
 then
 	echo please supply command
@@ -54,6 +79,10 @@ else
 	do
 		((my_count--))
 		eval $CMD
+		return_code=$?
+		RETURN_CODES[$return_code]=$(( RETURN_CODES[$return_code] + 1 ))
+		sleep 1
 	done
-
+	
+	summary 1
 fi
