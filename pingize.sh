5be742509eff388f6f0b028580240d05f106240d
bindkeeper <bindkeeper@gmail.com>
2015-08-01 15:59:53 +0300

    added the --sys-trace option it will create four log files for each failed execution: 1) io file will hold the disk io taken from /proc/<pid>/io 2) statem file will hold the snapshots of /proc/<pid>/statem 3) cpu file will hold the user mod and kernel mod times as well as threads, this information is taken from /proc/<pid>/stat 4) net file will hold printouts of ifconfig -s

--- pingize.sh
+++ pingize.sh
@@ -34,11 +34,83 @@ function usage {
 	echo -e '\t\t\t\t' command invocation attempts before stopping the script
 	echo -e '\t'	--net-trace '\t'	will create a pcap file for every failed execution
 	echo -e '\t\t\t' the names of the file will be [failed attempt number].pcap
+	echo -e '\t'	--sys-trace '\t'	will create four log files for every failed execution
+	echo -e '\t\t\t' the names of the files will be:
+	echo -e	'\t\t\t' [failed attempt number].io will hold the disk i\o information
+	echo -e	'\t\t\t' [failed attempt number].cpu will hold the cpu information and number of thread detected
+	echo -e	'\t\t\t' [failed attempt number].statm will hold the memory information
+	echo -e	'\t\t\t' [failed attempt number].net will hold the printouts of ifconfig -s
+
 	exit $1
+}
+
+function sys_trace_clean {
+
+	if [[ -e temp.io ]] ; then rm ./temp.io; fi
+	if [[ -e temp.statm ]] ; then rm ./temp.statm; fi
+	if [[ -e temp.cpu ]] ; then rm ./temp.cpu; fi
+	if [[ -e temp.net ]] ; then rm ./temp.net; fi
+}
+
+function sys_loging {
+	echo ====== `date` ===== >> temp.io
+	cat /proc/"${cmd_pid}"/io >> temp.io
+	echo =================end==================== >> temp.io
+
+	
+	echo ====== `date` ===== >> temp.statm
+	awk ' {printf "size\t\t%s\nresident\t%s\nshare\t\t%s\ntext\t\t%s\nlib\t\t%s\ndata\t\t%s\ndt\t\t%s\n", $1, $2, $3, $4, $5, $6, $7} ' /proc/"${cmd_pid}"/statm >> temp.statm
+	echo =================end==================== >> temp.statm
+
+	echo ====== `date` ===== >> temp.cpu
+	awk ' {printf "user mod cpu time (utime)\t%s\nkernel mod cpu time (stime)\t%s\nThreads\t\t\t\t%s\n", $14, $15, $20} ' /proc/"${cmd_pid}"/stat >> temp.cpu
+	echo =================end==================== >> temp.cpu
+
+	
+	echo ====== `date` ===== >> temp.net
+	ifconfig -s >> temp.net
+	echo =================end==================== >> temp.net
+
+}
+
+function run_command {
+	if [[ $SYS_TRACE ]]
+	then
+		sys_trace_clean
+		eval " ( $CMD ) & "
+		cmd_pid=$!
+		echo the cmd_pid is "${cmd_pid}"
+		while (( 1 ))
+		do 
+			kill -SIGSTOP $cmd_pid # pause the command
+		
+			sys_loging
+
+			kill -SIGCONT $cmd_pid # resumes the command
+			sleep 0.1
+			ps -ef | awk ' { print $2 } ' | grep $cmd_pid > /dev/null 2>&1 # search for the command process
+			if [[ $? -ne 0 ]]
+			then
+				# the command process not found so we stop the loop
+				break
+			fi
+		done
+
+		wait $cmd_pid # catching the return code of the command
+		return_code=$?
+
+	else
+		eval $CMD
+		return_code=$?
+	fi
 }
 
 function killed {
 	echo -e '\n'------ HEY! You interrupted me -----
+	if [[ $SYS_TRACE ]]
+	then
+		sys_trace_clean
+	fi
 	summary 0
 }
 
@@ -49,6 +121,7 @@ COUNT=
 CMD=
 N_COUNT=
 NET_TRACE=
+SYS_TRACE=
 
 # read user parameters
 while [[ $# > 0 ]] # travers on all parameters supplied to the script
@@ -81,6 +154,9 @@ do
 			exit 2
 		fi
 		NET_TRACE=1
+		;;
+		--sys-trace)
+		SYS_TRACE=1
 		;;
 		*) # default case
 		echo not valid option
@@ -120,16 +196,14 @@ else
 			tcpdump -w temp.pcap > /dev/null 2>&1 &
 			tcp_dump_pid=$!
 			sleep 1
-			eval $CMD
-			return_code=$?
+			run_command
 			sleep 1
 			kill $tcp_dump_pid
 		else
-			eval $CMD
-			return_code=$?
+			run_command
 		fi
 
-		if [[ ( ( $N_COUNT ) && ( $return_code -ne 0 ) ) || ( $NET_TRACE ) ]]
+		if [[ ( ( $N_COUNT ) && ( $return_code -ne 0 ) ) || ( $NET_TRACE ) || ( $SYS_TRACE ) ]]
 		then 
 			((my_n_count++))
 		fi
@@ -147,6 +221,22 @@ else
 				# we need to rename the temp.pcap
 				mv temp.pcap "${my_n_count}".pcap
 			fi
+		fi
+		
+		if [[ $SYS_TRACE ]]
+		then
+			if [[ $return_code -ne 0 ]]
+			then
+				mv temp.io "${my_n_count}".io
+				mv temp.statm "${my_n_count}".statm
+				mv temp.cpu "${my_n_count}".cpu
+				mv temp.net "${my_n_count}".net
+			else
+				rm temp.io
+				rm temp.statm
+				rm temp.cpu
+				rm temp.net
+			fi	
 		fi
 
 		RETURN_CODES[$return_code]=$(( RETURN_CODES[$return_code] + 1 ))
